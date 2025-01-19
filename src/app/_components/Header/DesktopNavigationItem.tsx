decfc6bb3d73cfd02212cd26759aa2c687f7fee8
Carlos Galindo <utm22030587@utma.edu.mx>
2025-01-19 01:26:53 -0600

    fix: set div with animation inside link

--- src/app/_components/Header/DesktopNavigationItem.tsx
+++ src/app/_components/Header/DesktopNavigationItem.tsx
@@ -7,17 +7,21 @@ export default function DesktopNavigationItem({ href, text }: { href: string, te
     const pathName = usePathname();
 
     return (
-        <motion.li
-            whileHover={{ scale: 1.1 }}
-            whileTap={{ scale: 0.9 }}
+        <li
             className="cursor-pointer"
         >
             <Link
                 href={href}
                 className={`hover:text-blue-200 ${pathName === href ? "text-blue-200" : ""}`}
             >
-                {text}
+                <motion.div
+                    whileHover={{ scale: 1.1 }}
+                    whileTap={{ scale: 0.9 }}
+
+                >
+                    {text}
+                </motion.div>
             </Link>
-        </motion.li>
+        </li>
     )
 }
