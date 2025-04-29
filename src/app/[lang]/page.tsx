b11fd9f9e368afb4eba1543850488426ef4dc892
Carlos Galindo <utm22030587@utma.edu.mx>
2025-04-29 13:39:29 -0600

    feat: redirect to english page when language does not exist

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -2,10 +2,15 @@ import languageEnglish from "@/dictionaries/en.json";
 import languageSpanish from "@/dictionaries/es.json";
 
 import Main from "../_components/Main";
+import { redirect } from "next/navigation";
 
 export default async function Page({ params }: { params: Promise<{ lang: string }> }) {
 
     const { lang } = await params;
+
+    if (!lang || (lang !== "es" && lang !== "en")) {
+        redirect("/en");
+    }
 
     const language = lang === "es" ? languageSpanish : languageEnglish;
 
