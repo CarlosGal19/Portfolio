232f647e94f431037e33c1166d7db4c254ed86aa
Carlos Galindo <c.galindo.islas@gmail.com>
2026-02-05 19:27:05 -0600

    feat: update stack, used technologies and projects order

--- src/app/[lang]/about/_components/About.tsx
+++ src/app/[lang]/about/_components/About.tsx
@@ -61,7 +61,7 @@ export default function About({ about }: { about: AboutProps }) {
                     className='container mx-auto flex justify-center px-4'
                 >
                     <Image
-                        src={`https://skillicons.dev/icons?i=ts,npm,postgres,tailwind,nest,next,docker&perline=${windowWidth > 768 ? 8 : 3}`}
+                        src={`https://skillicons.dev/icons?i=postgres,nest,angular,docker&perline=${windowWidth > 768 ? 8 : 3}`}
                         alt="IT SKILLS"
                         width={windowWidth > 768 ? 1000 : 500}
                         height={200}
@@ -83,7 +83,7 @@ export default function About({ about }: { about: AboutProps }) {
                     className='container mx-auto mb-12 flex justify-center px-4'
                 >
                     <Image
-                        src={`https://skillicons.dev/icons?i=react,js,ts,nodejs,npm,express,py,fastapi,mysql,mongodb,php,java,cs,tailwind,cpp,dotnet,next,prisma,postgres,linux,docker,nest&perline=${windowWidth > 768 ? 8 : 3}`}
+                        src={`https://skillicons.dev/icons?i=nest,ts,docker,postgres,cs,dotnet,angular,py,debian,kali,java,react,nodejs,next,prisma,fastapi,mysql,mongodb,php,tailwind,cpp&perline=${windowWidth > 768 ? 8 : 3}`}
                         alt="IT SKILLS"
                         width={windowWidth > 768 ? 1000 : 500}
                         height={200}
